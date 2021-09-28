db3bef8a0b262c8cdec57036dcb65e4a8cdca6d1
Nina Herman <nina.n.herman.22@dartmouth.edu>
2021-09-28 18:33:50 -0400

    created initial list using sampledata.js

---
+++ components/ListItem.js
@@ -0,0 +1,77 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+import React from 'react';
+import {
+    View, Text, StyleSheet, Image, TouchableOpacity,
+} from 'react-native';
+
+const ListItem = ({
+    name, symbol, currentPrice, priceChangePercentage7d, logoUrl,
+}) => {
+    const priceChangeColor = priceChangePercentage7d > 0 ? '#34C759' : '#FF3B30';
+    return (
+        <TouchableOpacity>
+            <View style={styles.itemWrapper}>
+
+                {/* left side */}
+                <View style={styles.leftWrapper}>
+                    <Image source={{ uri: logoUrl }} style={styles.image} />
+                    <View style={styles.titlesWrapper}>
+                        <Text style={styles.title}>{name}</Text>
+                        <Text style={styles.subtitle}>{symbol.toUpperCase()}</Text>
+                    </View>
+                </View>
+
+                {/* right side */}
+                <View style={styles.rightWrapper}>
+                    <Text style={styles.title}>
+                        $
+                        {currentPrice.toLocaleString('en-US', { current: 'USD' })}
+                    </Text>
+                    <Text style={[styles.subtitle, { color: priceChangeColor }]}>
+                        {priceChangePercentage7d.toFixed(2)}
+                        %
+                    </Text>
+                </View>
+
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    itemWrapper: {
+        paddingHorizontal: 16,
+        marginTop: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    leftWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        height: 48,
+        width: 48,
+    },
+    titlesWrapper: {
+        marginLeft: 8,
+    },
+    rightWrapper: {
+        alignItems: 'flex-end',
+    },
+    title: {
+        fontSize: 18,
+    },
+    subtitle: {
+        marginTop: 4,
+        fontSize: 14,
+        color: '#A9ABB1',
+    },
+});
+
+export default ListItem;
